50af8e8ff699a75695743592e817ac15866c86e8
biannasantana07 <biannasantana07@gmail.com>
2024-06-24 00:22:19 +0000

    cambio currencypipe

--- src/app/resultado-roi/resultado-roi.component.ts
+++ src/app/resultado-roi/resultado-roi.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RoiService } from '../services/roi.service';
 import { inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-resultado-roi',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './resultado-roi.component.html',
   styleUrl: './resultado-roi.component.css'
 })
